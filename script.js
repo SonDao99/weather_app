ba857546f1905c0761c750cc7887f1110f5a6178
Son Dao <sondao1999@gmail.com>
2022-02-26 09:47:38 +0700

    changed http to https on links

--- script.js
+++ script.js
@@ -39,9 +39,9 @@ const weatherApp = (() => {
         let response
         try {
             if (currentUnits === 'metric') {
-                response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityString}&units=metric&APPID=d2c0c1fef0683f8a6eff0a63e9b895cd`, {mode:'cors'});
+                response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityString}&units=metric&APPID=d2c0c1fef0683f8a6eff0a63e9b895cd`, {mode:'cors'});
             } else {
-                response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityString}&units=imperial&APPID=d2c0c1fef0683f8a6eff0a63e9b895cd`, {mode:'cors'});
+                response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityString}&units=imperial&APPID=d2c0c1fef0683f8a6eff0a63e9b895cd`, {mode:'cors'});
             }
 
             let weatherData = await response.json();
@@ -106,21 +106,21 @@ const weatherApp = (() => {
     function displayWeatherIcon(data) {
         if (data.mainDescription.toLowerCase() === 'rain') {
             weatherIcon.innerHTML = 
-                `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-cloud-rain" width="100" height="100" viewBox="0 0 24 24" stroke-width="0.5" stroke="#00bfd8" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                `<svg xmlns="https://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-cloud-rain" width="100" height="100" viewBox="0 0 24 24" stroke-width="0.5" stroke="#00bfd8" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7" />
                     <path d="M11 13v2m0 3v2m4 -5v2m0 3v2" />
                 </svg>`;
         } else if (data.mainDescription.toLowerCase() === 'snow') {
             weatherIcon.innerHTML = 
-                `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-cloud-snow" width="100" height="100" viewBox="0 0 24 24" stroke-width="0.5" stroke="#00bfd8" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                `<svg xmlns="https://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-cloud-snow" width="100" height="100" viewBox="0 0 24 24" stroke-width="0.5" stroke="#00bfd8" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7" />
                     <path d="M11 15v.01m0 3v.01m0 3v.01m4 -4v.01m0 3v.01" />
                 </svg>`;
         } else if (data.mainDescription.toLowerCase() === 'mist') {
             weatherIcon.innerHTML = 
-                `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mist" width="100" height="100" viewBox="0 0 24 24" stroke-width="0.5" stroke="#00bfd8" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                `<svg xmlns="https://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mist" width="100" height="100" viewBox="0 0 24 24" stroke-width="0.5" stroke="#00bfd8" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M5 5h3m4 0h9" />
                     <path d="M3 10h11m4 0h1" />
@@ -129,20 +129,20 @@ const weatherApp = (() => {
                 </svg>`;
         } else if (data.mainDescription.toLowerCase() === 'clouds') {
             weatherIcon.innerHTML = 
-                `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-cloud" width="100" height="100" viewBox="0 0 24 24" stroke-width="0.5" stroke="#00bfd8" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                `<svg xmlns="https://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-cloud" width="100" height="100" viewBox="0 0 24 24" stroke-width="0.5" stroke="#00bfd8" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7h-12" />
                 </svg>`;
         } else if (data.mainDescription.toLowerCase() === 'clear') {
             if (data.currentTime < data.sunrise || data.currentTime > data.sunset) {
                 weatherIcon.innerHTML = 
-                    `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon" width="100" height="100" viewBox="0 0 24 24" stroke-width="0.5" stroke="#00bfd8" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    `<svg xmlns="https://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon" width="100" height="100" viewBox="0 0 24 24" stroke-width="0.5" stroke="#00bfd8" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
                     </svg>`;
             } else {
                 weatherIcon.innerHTML = 
-                    `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun" width="100" height="100" viewBox="0 0 24 24" stroke-width="0.5" stroke="#ffbf00" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    `<svg xmlns="https://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun" width="100" height="100" viewBox="0 0 24 24" stroke-width="0.5" stroke="#ffbf00" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <circle cx="12" cy="12" r="4" />
                         <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
